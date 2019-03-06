4e6a0529fc44037709dbb8989622d57c9014c16b
Agustin Martin Domingo <agustin6martin@gmail.com>
2019-03-06 15:50:56 +0100

    call_Moody.py: First cut for a python script calling MoodyChart.py python module.
    Signed-off-by: Agustin Martin Domingo <agustin6martin@gmail.com>

---
+++ call_Moody.py
@@ -0,0 +1,25 @@
+import matplotlib as mpl
+mpl.use("pgf")
+
+pgf_with_pdflatex = {
+    "pgf.texsystem": "pdflatex",
+    "font.family": "serif",
+    "pgf.preamble": [
+        r"\usepackage[utf8x]{inputenc}",
+        r"\usepackage[T1]{fontenc}",
+        r"\usepackage{times}",
+    ]
+}
+mpl.rcParams.update(pgf_with_pdflatex)
+
+# Output file basename
+outputname = "example_plot"
+
+import MoodyChart as MC
+
+plt = MC.MoodyChart()
+ax  = plt.gca()    # Only needed if something else is to be plotted
+
+# Plotting ...
+plt.savefig(outputname + '.pgf', bbox_inches = 'tight')
+plt.savefig(outputname + '.pdf', bbox_inches = 'tight')
