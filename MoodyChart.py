e8089bb98b87d2f385bc7602f1946934d41fe8c2
Agustin Martin Domingo <agustin6martin@gmail.com>
2019-06-27 18:01:00 +0200

    MoodyChart.py: Use the correct name for GNU Lesser General Public License.
    Signed-off-by: Agustin Martin Domingo <agustin6martin@gmail.com>

--- MoodyChart.py
+++ MoodyChart.py
@@ -1,8 +1,9 @@
-# MoodyChart.py - A Python module to play with  Moody charts
-# Copyright (C) 2018-2019 Agustin Martin Domingo
+# MoodyChart.py - A Python module to play with Moody charts
+#
+# Copyright (C) 2018-2019 Agustin Martin Domingo <agustin6martin@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
+# modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 3 of the License, or (at your option) any later version.
 #
@@ -11,7 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General Public
+# You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see
 # <http://www.gnu.org/licenses/>.
 
