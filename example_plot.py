5ed8b8a167a373fd0599037e94910546e9262dd7
Agustin Martin Domingo <agustin6martin@gmail.com>
2021-02-26 14:17:01 +0100

    example_plot.py: Cosmetic changes to pgf.preamble string.
    This should make it more readable.
    
    Signed-off-by: Agustin Martin Domingo <agustin6martin@gmail.com>

--- example_plot.py
+++ example_plot.py
@@ -4,7 +4,10 @@ mpl.use("pgf")
 pgf_with_pdflatex = {
     "pgf.texsystem": "pdflatex",
     "font.family": "serif",
-    "pgf.preamble": r"\usepackage[utf8x]{inputenc} \usepackage[T1]{fontenc} \usepackage{times}"
+    "pgf.preamble": \
+         r"\usepackage[utf8x]{inputenc}" \
+         r"\usepackage[T1]{fontenc}" \
+         r"\usepackage{times}"
 }
 mpl.rcParams.update(pgf_with_pdflatex)
 
